ffd1e64fa0618387d4c746be87c6bf9a78b5f7af
mrustl <michael.rustler@kompetenz-wasser.de>
2020-06-03 18:39:17 +0200

    Add function create_partners_budget_files()

--- R/.read_budget-files.R
+++ R/.read_budget-files.R
@@ -31,62 +31,12 @@ if (FALSE)
     "proposals/h2020_covid/60_Budget/DWH_partner-budget_template.xlsx",
      target_dir = dirname(path_partners))
 
-create_partners_budget_files <- function(path_partners,
-                                         path_budget_template,
-                                         target_dir = file.path(dirname(path_partners),
-                                                                 "10_Filled_out_forms"),
-                                         set_values = FALSE,
-                                         overwrite = TRUE) {
+output_dir <- create_partners_budget_files(path_partners,
+                                           path_budget_template,
+                                           target_dir = file.path(dirname(path_partners),
+                                                                  "10_Filled_out_forms"),
+                                           set_values = TRUE)
 
-
-
- partner_infos <- openxlsx::read.xlsx(xlsxFile = path_partners,
-                                      sheet = "Partners-PIC-Main contact")
-
- fs::dir_create(target_dir)
-
- if (set_values)  wb <- openxlsx::loadWorkbook(path_budget_template)
-
-message(sprintf("Creating target directory: %s", target_dir))
-sapply(seq_len(nrow(partner_infos)), function(index) {
-
-  metadata <-  partner_infos[index,]
-
-  budget_file_name <- sprintf("DWH_partner-budget_%02d_%s.xlsx",
-                              metadata$partner_id,
-                              metadata$partner_name_short)
-
-  target_file <- file.path(target_dir, budget_file_name)
-
-
-  if (set_values) {
-   wb <- openxlsx::loadWorkbook(path_budget_template)
-   message(sprintf("Renaming template and add partner metadata (DANGER: cell protection is lost!): %s",
-                   target_file))
- openxlsx::writeData(wb = wb,
-                     sheet = "Summary",
-                     x = c(metadata$pic,
-                           metadata$partner_name_legal,
-                           metadata$partner_name_short,
-                           metadata$funding_rate),
-                     startCol = "C",
-                     startRow = 5)
-
-
-
- openxlsx::saveWorkbook(wb, file = target_file, overwrite = overwrite)
-  } else {
-    message(sprintf("Renaming budget-template to: %s", target_file))
-    fs::file_copy(path_budget_template, target_file, overwrite = overwrite)
-  }
-
-
-})
-
-target_dir
-}
-
-output_dir <- create_partners_budget_files(path_partners, path_budget_template, set_values = TRUE)
 kwb.utils::hsOpenWindowsExplorer(output_dir)
 
  #path <- "proposals/bmbf_digital/Previous-projects/Budget"
