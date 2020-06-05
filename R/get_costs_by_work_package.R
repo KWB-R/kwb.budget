899bdd9c6757e8f9b3cfe0934d4cc2fe4e26ea0a
mrustl <michael.rustler@kompetenz-wasser.de>
2020-06-05 02:12:01 +0200

    Fix :book:

--- R/get_costs_by_work_package.R
+++ R/get_costs_by_work_package.R
@@ -1,27 +1,27 @@
-##' Helper function: append zero costs
-##' @param x x
-#'  @param n_work_packages number of work packages in EXCEL template
+#' Helper function: append zero costs
+#' @param x x
+#' @param n_work_packages number of work packages in EXCEL template
 #' (default: 7, as used for DWC)
-##' @return data frame with zero costs
-##' @export
-##' @importFrom kwb.utils noFactorDataFrame safeRowBind
-##'
+#' @return data frame with zero costs
+#' @export
+#' @importFrom kwb.utils noFactorDataFrame safeRowBind
+#'
 append_zero_costs <- function(x, n_work_packages) {
   kwb.utils::safeRowBind(x, kwb.utils::noFactorDataFrame(
     partner = unique(x$partner), wp = seq_len(n_work_packages), cost = 0
   ))
 }
 
-##' Get Costs by Work Package
-##' @param costs_list list with "costs" from multiple partner Excelsheets
-#'  @param n_work_packages number of work packages in EXCEL template
+#' Get Costs by Work Package
+#' @param costs_list list with "costs" from multiple partner Excelsheets
+#' @param n_work_packages number of work packages in EXCEL template
 #' (default: 7, as used for DWC)
-##' @return data frame with costs per work package
-##' @export
-##' @importFrom kwb.utils defaultIfNA getAttribute rbindAll
-##' @importFrom dplyr group_by summarise
-##' @importFrom rlang .data
-##'
+#' @return data frame with costs per work package
+#' @export
+#' @importFrom kwb.utils defaultIfNA getAttribute rbindAll
+#' @importFrom dplyr group_by summarise
+#' @importFrom rlang .data
+#'
 get_costs_by_work_package <- function(costs_list, n_work_packages = 7)
 {
   # costs_list must be a list
