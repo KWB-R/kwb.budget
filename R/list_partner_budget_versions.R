d4bd125b4bb392831d23cede6acfe75e398a2ac1
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-06-05 09:27:25 +0200

    Select main columns and order columns

--- R/list_partner_budget_versions.R
+++ R/list_partner_budget_versions.R
@@ -9,7 +9,21 @@
 list_partner_budget_versions <- function()
 {
   path <- "proposals/h2020_covid/60_Budget/10_Filled_out_forms"
-  (file_info <- kwb.nextcloud::list_files(path, full_info = TRUE))
+
+  file_info <- kwb.nextcloud::list_files(path, full_info = TRUE)
+
   version_info <- kwb.nextcloud::get_version_info(file_info$fileid)
-  merge(file_info[, 1:5], version_info, by = "fileid")
+
+  columns_x <- c("fileid", "file", "lastmodified", "etag")
+  columns_y <- c("fileid", "version", "href")
+
+  result <- merge(
+    kwb.utils::selectColumns(file_info, columns_x),
+    kwb.utils::selectColumns(version_info, columns_y),
+    by = "fileid"
+  )
+
+  kwb.utils::moveColumnsToFront(result, columns = c(
+    "fileid", "file", "version"
+  ))
 }
