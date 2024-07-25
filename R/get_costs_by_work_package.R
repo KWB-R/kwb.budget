b19d10c535325b1f51e6e8b0d191ee940f8e2de4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-07-25 23:34:36 +0200

    Use .groups = "drop, mergeAll() silently

--- R/get_costs_by_work_package.R
+++ R/get_costs_by_work_package.R
@@ -50,16 +50,22 @@ get_costs_by_work_package <- function(costs_list, n_work_packages = 7)
   sum_by_work_package <- function(x) {
     x %>%
       dplyr::group_by(.data$partner, .data$wp) %>%
-      dplyr::summarise(cost = sum(.data$cost))
+      dplyr::summarise(
+        cost = sum(.data$cost),
+        .groups = "drop"
+      )
   }
 
   sum_pm_by_work_package <- function(x) {
     x %>%
       dplyr::group_by(.data$partner, .data$wp) %>%
-      dplyr::summarise(person_months = sum(.data$person_months, na.rm = TRUE))
+      dplyr::summarise(
+        person_months = sum(.data$person_months, na.rm = TRUE),
+        .groups = "drop"
+      )
   }
 
-  kwb.utils::mergeAll(by = c("partner", "wp"), list(
+  kwb.utils::mergeAll(by = c("partner", "wp"), dbg = FALSE, list(
     personnel = sum_pm_by_work_package(personnel),
     personnel = sum_by_work_package(personnel),
     equipment = sum_by_work_package(equipment),
