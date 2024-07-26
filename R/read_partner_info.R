dd3502a1c2daf6d2bae8e461ea659580d89cff15
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-07-26 21:50:40 +0200

    Set default column names

--- R/read_partner_info.R
+++ R/read_partner_info.R
@@ -9,12 +9,21 @@
 #' @param sheet name of Excel sheet
 #' @param local_path if given, the Excel file is read from this (local) path
 #' @param columns vector of column names. If given, only these columns are
-#'  returned
+#'   returned. The default is this vector of required column names:
+#'  \code{c("partner_id", "partner_name_short", "partner_name_legal", "pic",
+#'  "funding_rate")}
 #' @return data frame with attribute "local_path" being set to the path to the
 #'   local Excel file from which the data were read
 #' @export
 read_partner_info <- function(
-    nextcloud_path, sheet, local_path = NULL, columns = NULL
+    nextcloud_path, sheet, local_path = NULL,
+    columns = c(
+      "partner_id",
+      "partner_name_short",
+      "partner_name_legal",
+      "pic",
+      "funding_rate"
+    )
 )
 {
   if (is.null(local_path)) {
