169a5621382c4601f73fac0239631175639d5f2e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-07-29 16:51:31 +0200

    Set default of argument "columns" to NULL

--- R/read_partner_info.R
+++ R/read_partner_info.R
@@ -9,30 +9,33 @@
 #' @param sheet name of Excel sheet
 #' @param local_path if given, the Excel file is read from this (local) path
 #' @param columns vector of column names. If given, only these columns are
-#'   returned. The default is this vector of required column names:
-#'  \code{c("partner_id", "partner_name_short", "partner_name_legal", "pic",
-#'  "funding_rate")}
+#'   returned.
 #' @return data frame with attribute "local_path" being set to the path to the
 #'   local Excel file from which the data were read
 #' @export
 read_partner_info <- function(
-    nextcloud_path, sheet, local_path = NULL,
-    columns = c(
-      "partner_id",
-      "partner_name_short",
-      "partner_name_legal",
-      "pic",
-      "funding_rate"
-    )
+    nextcloud_path, sheet, local_path = NULL, columns = NULL
 )
 {
   if (is.null(local_path)) {
     local_path <- kwb.nextcloud::download_files(paths = nextcloud_path)
   }
 
-  local_path %>%
+  required_columns <- c(
+    "partner_id",
+    "partner_name_short",
+    "partner_name_legal",
+    "pic",
+    "funding_rate"
+  )
+
+  data <- local_path %>%
     kwb.utils::safePath() %>%
-    openxlsx::read.xlsx(sheet = sheet) %>%
+    openxlsx::read.xlsx(sheet = sheet)
+
+  kwb.utils::checkForMissingColumns(data, required_columns)
+
+  data %>%
     kwb.utils::selectColumns(columns) %>%
     structure(local_path = local_path)
 }
